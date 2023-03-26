018fb62b071a52ecffacd83d172965cef7da60f0
Mohd Imran Khan <79039055+codingbaba1@users.noreply.github.com>
2023-03-26 19:42:05 +0530

    Delete airlineresevationsystem directory

--- airlineresevationsystem/airlineresevationsystem/src/main/java/com/ars/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ars.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ars.entity.Passenger;
-
-//passenger repository interface
-public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
-}
